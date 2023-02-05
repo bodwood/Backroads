ea489bf3ff20d9149be1a07193f198e22f1a9974
bodwood <bodiewood@hotmail.com>
2023-02-05 10:46:52 -0800

    add dynamic mapping for social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import logo from '../images/logo.svg'
 import { pageLinks } from '../data'
+import { socialLinks } from '../data'
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
@@ -24,33 +26,17 @@ const Navbar = () => {
         </ul>
 
         <ul className='nav-icons'>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-facebook'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-twitter'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-squarespace'></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a
+                 href={link.href} target={link.target} className={link.aclassName}>
+                  <i className={link.iclassName}></i>
+                </a>
+              </li>
+            )
+          })}
+
         </ul>
       </div>
     </nav>
--- src/data.js
+++ src/data.js
@@ -4,3 +4,27 @@ export const pageLinks = [
   { id: 3, href: '#services', text: 'services' },
   { id: 4, href: '#tours', text: 'tours'}
 ];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'https://www.facebook.com',
+    target: '_blank',
+    aclassName: 'nav-icon',
+    iclassName: 'fab fa-facebook',
+  },
+  {
+    id: 2,
+    href: 'https://www.twitter.com',
+    target: '_blank',
+    aclassName: 'nav-icon',
+    iclassName: 'fab fa-twitter',
+  },
+  {
+    id: 3,
+    href: 'https://www.squarespace.com',
+    target: '_blank',
+    aclassName: 'nav-icon',
+    iclassName: 'fab fa-squarespace',
+  },
+]
